49c88227fdc5f596b8e4ea9f1904e39af0153022
Harven <Harven@DESKTOP-088RGA8>
2024-02-04 23:06:12 +0800

    Add project files.

---
+++ Activity2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter length(in centimeters)>>> ");
+int cm = Convert.ToInt32(Console.ReadLine());
+int yards, foot, inches;
+yards = (int)(cm / 91.44);
+foot = (int)((cm % 91.44) / 30.48);
+inches = (int)((cm % 91.44) % 30.48 / 2.54);
+Console.WriteLine(yards + " yards, " + foot + " feet (foot), " + inches + " inches");
